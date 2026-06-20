f7ded494810a2e6842b5e7f196431d5206f3d285
anandabuddannagari <ananda.buddannagari@gmail.com>
2026-06-20 09:54:50 -0400

    Add files via upload

---
+++ afterclass.py
@@ -0,0 +1,13 @@
+print("=== My Daily Routine ===")
+
+print("1. Wake up at 7:00 AM")
+print("2. Eat breakfast")
+print("3. Go to school")
+print("4. Come home and play")
+print("5. Study for 2 hours")
+
+# Weekly study time calculation
+daily_study = 2
+weekly_study = daily_study * 7
+
+print("Total study time in a week:", weekly_study, "hours")
