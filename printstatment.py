7bb1327883350a1f9ed2fdaa33a6cbc6a9a12ff9
anandabuddannagari <ananda.buddannagari@gmail.com>
2026-06-20 09:29:26 -0400

    Add files via upload

---
+++ printstatment.py
@@ -0,0 +1,12 @@
+#Printing a greeting message
+print("Hello,World")
+
+#Printing my name
+print("My name is Alex")
+
+#Printing a fun fact about myself
+#print("I love playing soccer.")
+print("I love playing soccer.")
+
+#Performing and printing a simple calculation
+print("result of 3 + 5 is :", 3 + 5)
