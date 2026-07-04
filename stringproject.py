4a45005e7f590e924dc492761685c4bf77eaf508
anandabuddannagari <ananda.buddannagari@gmail.com>
2026-07-04 09:58:41 -0400

    Add files via upload

---
+++ stringproject.py
@@ -0,0 +1,10 @@
+# This program changes a word from lowercase to UPPERCASE
+
+# Ask the user to type a word
+word = input("Type a word in lowercase: ")
+
+# Convert the word to uppercase
+upper_word = word.upper()
+
+# Show the result
+print("Here is your word in uppercase:", upper_word)
