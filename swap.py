7864601602eb97aadb322538eb0c81b0605eb2f5
anandabuddannagari <ananda.buddannagari@gmail.com>
2026-07-25 09:56:19 -0400

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,15 @@
+a = 5
+b = 10
+c = 15
+
+print("Before swapping:")
+print(a, b, c)
+
+# Swapping using a temporary variable
+temp = a
+a = b
+b = c
+c = temp
+
+print("After swapping:")
+print(a, b, c)
