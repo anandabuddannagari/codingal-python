e109e7ee2f4703283ef381f48f8dcc21838182f1
anandabuddannagari <ananda.buddannagari@gmail.com>
2026-09-19 09:44:38 -0400

    Add files via upload

---
+++ countrycode.py
@@ -0,0 +1,11 @@
+country_code = {'india' : '0091'
+                'Australia':'0025',
+                'Nepal':'00977'
+
+#search dictionary for country code of india
+print("country code for india -")
+print(country_code.get('india,''Not found'))
+
+#search dictionary for country code of japan
+print("country code for Japan -")
+print(country_code.get('Japan','not found'))
